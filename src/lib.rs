75440d80df3062073ec799fb85ae463cf5cfcc40
gracedemos <graysondemos@gmail.com>
2024-07-28 00:22:00 -0400

    Added account close function

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ pub fn process_transaction(program_id: &Pubkey, accounts: &[AccountInfo], data: 
     let account_len = data.len();
 
     if account_len == 0 {
-        return close_account(signer, pda_account);
+        return close_account(signer, pda_account, program_id);
     }
 
     let rent = Rent::get()
@@ -71,7 +71,22 @@ pub fn process_transaction(program_id: &Pubkey, accounts: &[AccountInfo], data: 
     Ok(())
 }
 
-fn close_account(signer: &AccountInfo, pda: &AccountInfo) -> Result<(), ProgramError> {
+fn close_account(signer: &AccountInfo, pda: &AccountInfo, program_id: &Pubkey) -> Result<(), ProgramError> {
+    let image: Image = bincode::deserialize(&pda.data.borrow()).unwrap();
+
+    let (derived_pda, _bump_seed) = Pubkey::find_program_address(
+        &[signer.key.as_ref(), image.title.as_bytes()],
+        program_id
+    );
+
+    if derived_pda.to_string() != pda.key.to_string() {
+        msg!("Error: Invalid Signer");
+        msg!("Input PDA: {}", pda.key);
+        msg!("Derived PDA: {}", derived_pda);
+
+        return Err(ProgramError::IllegalOwner);
+    }
+
     **signer.try_borrow_mut_lamports()? += pda.lamports();
     **pda.try_borrow_mut_lamports()? = 0;
 
